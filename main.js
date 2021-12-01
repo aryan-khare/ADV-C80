3e9e1368d243cbb0e5e8a050f74bdf06efd8ddec
aryan-khare <89584494+aryan-khare@users.noreply.github.com>
2021-12-01 17:34:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,32 @@
-var names_of_people = [];
-    
-function submit()
-{
-    var GuestName = document.getElementById("name1").value;
-	// use the push function as - names_of_people.push(GuestName);
-	console.log(GuestName);    
-    console.log(names_of_people);
-    var lenght_of_name = names_of_people.length;
-    console.log(lenght_of_name);
-	document.getElementById("display_name").innerHTML=names_of_people.toString();
-   }
-
-
-
-function show()
-{
-	var i= names_of_people.join("<br>");
-	console.log(names_of_people);
-	document.getElementById("p1").innerHTML=i.toString();
-	document.getElementById("     ").style.display="block";
-	// display the name in the id= sort_button
-	}
-
-
-function sorting()
-	{
-		names_of_people           // add the sort function
-		// .sort();
-		var i= names_of_people.join("<br>");
-		console.log(names_of_people);		
-		document.getElementById("sorted").innerHTML=i.toString();
-		}
-
-
-function searching()
-{
-	var s= document.getElementById("s1").value;
-	var found=0;
-	var j;
-	for(j=0; j<names_of_people.length; j++)
-		{
-			if(s==names_of_people[j]){
-				found=found+1;
-			}	
-		}
-	document.getElementById("p2").innerHTML="name found "+found+" time/s";
-	console.log("found name "+found+" time/s");
+var Name_Of_The_Guest_Array = [];
+function Submit() {
+   var Guest_name = document.getElementById("Enter_name").value;
+   Name_Of_The_Guest_Array.push(Guest_name);
+   document.getElementById("Display_names").innerHTML = Name_Of_The_Guest_Array;
+   console.log(Name_Of_The_Guest_Array);
+   var length_of_the_array = Name_Of_The_Guest_Array.length;
+    console.log(length_of_the_array);
+}
+function Show_list() {
+    var i = Name_Of_The_Guest_Array.join("<br>");
+    console.log(Name_Of_The_Guest_Array);
+    document.getElementById("Show_names").innerHTML = i.toString();
+}
+function sorting() {
+    Name_Of_The_Guest_Array.sort();
+    var i = Name_Of_The_Guest_Array.join("<br>");
+    console.log(Name_Of_The_Guest_Array);
+    document.getElementById("Sorted_names").innerHTML = i.toString();
+}
+function Search() {
+    var s = document.getElementById("Search_name").value;
+    var found = 0;
+    var j;
+    for(j=0; j<Name_Of_The_Guest_Array.length; j++) {
+        if(s==Name_Of_The_Guest_Array[j]) {
+            found = found+1;
+        }
+    }
+    document.getElementById("Search_names").innerHTML = "Name Found "+found+" Time/s";
+    console.log("Found Name "+found+" Time/s");
 }
